fc66512f0aa80e0a1da8915c44c6a0fab09a68f5
Eimrine's Point of View <eimrine@users.noreply.github.com>
2015-05-11 15:43:57 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,10 @@
+var ar1 = [1, 5, 6, 4, 3, 5, 100, -20];
+  function funDo(ar){
+  for(k=0,i=1,temp = ar.length-1;k<temp;k++)i=(i<<
+1)+1;
+  for(;i>0;i--){
+  for(k=0,sum=0,temp2=[];k<ar.length;k++)
+  if((i>>(k))%2) { sum+=ar[k]; temp2.push(ar[k]);}
+  if(sum==10) console.log(temp2);}
+  }
+  funDo(ar1);
